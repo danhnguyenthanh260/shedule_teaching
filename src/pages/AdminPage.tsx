c86fceb7cbad9ab74dc89c6a328fe9f1f4268263
Ngô Hoàng Trường Đạt <ngohoangtruongdat2@gmail.com>
2026-03-03 08:52:54 +0700

    thêm mail gv

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -369,7 +369,7 @@ export const AdminPage: React.FC = () => {
             return;
         }
 
-        const generatedEmail = `${newLecturer.code.trim()}@gmail.com`.toLowerCase();
+        const generatedEmail = `${newLecturer.code.trim()}@fpt.edu.vn`.toLowerCase();
         
         try {
             setLoading(true);
@@ -453,7 +453,7 @@ export const AdminPage: React.FC = () => {
                     if (emailIdx !== -1 && row[emailIdx]) {
                         email = String(row[emailIdx]).trim().toLowerCase();
                     } else if (code) {
-                        email = `${code}@gmail.com`.toLowerCase();
+                        email = `${code}@fpt.edu.vn`.toLowerCase();
                     }
 
                     if (name && code && email) {
